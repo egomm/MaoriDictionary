3206b26f890660915ab2f2abd29ba0cca05c0d2e
egomm <egomyt@gmail.com>
2023-03-22 10:16:16 +1300

    work in process (working through making home screen)

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():  # put application's code here
-    return render_template('home.html')
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
