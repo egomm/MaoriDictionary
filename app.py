42a076584de5953039933010c2c9fd1305fe9bd2
egomm <egomyt@gmail.com>
2023-03-24 14:20:41 +1300

    need to work on search bot at add background image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():  # put application's code here
-    return render_template('contact.html')
+    return render_template('newhome.html')
 
 
 if __name__ == '__main__':
