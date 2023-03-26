8be2201db6055fce60e12179010a47d55df67ff3
egomm <egomyt@gmail.com>
2023-03-26 17:24:26 +1300

    Still needs a lot of design work but is better

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():  # put application's code here
     return render_template('newhome.html')
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    print(request.form.get("text"))
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
