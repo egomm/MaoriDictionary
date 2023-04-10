5e33177b34377f29a9d39c4ea519922b1f60bce9
egomm <egomyt@gmail.com>
2023-04-11 00:08:33 +1200

    home page sorted - need to work on search

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -8,10 +8,18 @@ def home():  # put application's code here
     return render_template('newhome.html')
 
 
+# make the request go under a custom thing
 @app.route('/contact', methods=['POST', 'GET'])
 def contact():
     print(request.form.get("text"))
+    print(len(request.form.get("text")))
     return render_template('contact.html')
+
+
+@app.route('/translate/<word>', methods=['POST', 'GET'])
+def translate():
+     # need to validate the search
+     return render_template('translate.html')
 
 
 if __name__ == '__main__':
