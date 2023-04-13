9c162cc51d5d036522ac12e9db41e84f90e42b2e
egomm <egomyt@gmail.com>
2023-04-13 23:38:06 +1200

    Login in the works, need to format it

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+from sqlite3 import Error
+from flask_bcrypt import Bcrypt
+
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+DATABASE = os.path.join(PROJECT_ROOT, 'maoridictionary.db')
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.secret_key = "GiJHEtydYtpCIVSeCchIE43rScpzqnKU"
 
 
-@app.route('/')
+def open_database(db_name):
+    """
+    Create a connection with the database
+    parameter: name of the database file
+    returns: a connection to the file
+    """
+    try:
+        connection = sqlite3.connect(db_name)
+        return connection
+    except Error as e:
+        print(e)
+    return None
+
+
+def create_connection(db_file):
+    """
+    Create a connection with the database
+    parameter: name of the database file
+    returns: a connection of the file
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
+
+
+@app.route('/', methods=['POST', 'GET'])
 def home():  # put application's code here
     return render_template('newhome.html')
 
