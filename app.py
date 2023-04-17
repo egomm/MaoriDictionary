05be04469b7c91346c406554dbce6d2140bfae71
egomm <egomyt@gmail.com>
2023-04-18 00:06:54 +1200

    experimenting with context_processor (promising)

--- app.py
+++ app.py
@@ -8,10 +8,20 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DATABASE = os.path.join(PROJECT_ROOT, "maoridictionary.db")
+#DATABASE = "C:/Users/egomy/OneDrive/Documents/GitHub/MaoriDictionary/maoridictionary.db"
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key = "GiJHEtydYtpCIVSeCchIE43rScpzqnKU"
+
+
+@app.context_processor
+def inject_data():
+    hello = "hello"
+    if request.form.get("signup-form") is not None:
+        print(request.form.get("signup-username"))
+        print(request.form.get("teacherButton"))
+    return {"hello": hello}
 
 
 def open_database(db_name):
@@ -36,20 +46,33 @@ def create_connection(db_file):
     """
     try:
         connection = sqlite3.connect(db_file)
+        print(connection)
         return connection
     except Error as e:
         print(e)
     return None
+
+#def signUp(teacher, firstName, lastName, username, email, password):
 
 
 @app.route('/', methods=['POST', 'GET'])
 def home():  # put application's code here
     print(request.form.get("login-form") is not None)
     print(request.form.get("signup-form") is not None)
-    if request.form.get("signup-form") is not None:
-        print(request.form.get("signup-username"))
-        print(request.form.get("teacherButton"))
-    return render_template('home.html', text="hi")
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    query = "SELECT username FROM users"
+    cur.execute(query)
+    usernames = cur.fetchall()
+    query = "SELECT email FROM users"
+    cur.execute(query)
+    emails = cur.fetchall()
+    print(usernames)
+    print(emails)
+    #if request.form.get("signup-form") is not None:
+        #print(request.form.get("signup-username"))
+        #print(request.form.get("teacherButton"))
+    return render_template('home.html')
 
 
 @app.route('/categories', methods=['POST', 'GET'])
